1d080902c1eada8d7aa030151558b34169ebe1f2
Rajaabpro <rajaab.pro@gmail.com>
2024-11-04 20:49:47 +0500

    Add route to retrieve and display all blogs from the database

--- app.js
+++ app.js
@@ -1,70 +1,90 @@
-const express = require("express");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-const Blog = require("./models/blog");
-const { result } = require("lodash");
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
 // express app
 const app = express();
 
-// connect to mongodb
-const dbURI =
-  "mongodb+srv://rajaabpro:test123@cluster0.a8mneoq.mongodb.net/node-tuts?retryWrites=true&w=majority&appName=Cluster0";
-mongoose
-  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+// connect to mongodb & listen for requests
+const dbURI = "mongodb+srv://rajaabpro:test123@cluster0.a8mneoq.mongodb.net/node-tuts?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
+
 // register view engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
-//  middleware & static files
-app.use(express.static("public"));
-app.use(morgan("dev"));
+// middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
-//  mongoose and mongo sandbox routes
-app.get("/add-blog", (req, res) => {
+// mongoose & mongo tests
+app.get('/add-blog', (req, res) => {
   const blog = new Blog({
-    title: "new blog",
-    snippet: "about my new blog",
-    body: "more aboutmy new blog",
-  });
-  blog
-    .save()
-    .then((result) => {
+    title: 'new blog',
+    snippet: 'about my new blog',
+    body: 'more about my new blog'
+  })
+
+  blog.save()
+    .then(result => {
       res.send(result);
     })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
     });
 });
 
-app.use((req, res, next) => {
-  console.log("new request made:");
-  console.log("host: ", req.hostname);
-  console.log("path:", req.path);
-  console.log("method:", req.method);
-  next();
-});
-app.use((req, res, next) => {
-  console.log("in the next middleware");
-
-  next();
-});
-app.get("/", (req, res) => {
-  const blogs = [
-    { title: "Hi im vhvv ", snippet: "Im learing" },
-    { title: "Hi im Rajaab", snippet: "Im learing" },
-    { title: "Hi im Rajaab", snippet: "Im learing" },
-  ];
-  res.render("index", { title: "Home", blogs });
+app.get('/all-blogs', (req, res) => {
+  Blog.find()
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
-app.get("/about", (req, res) => {
-  res.render("about", { title: "About" });
+app.get('/single-blog', (req, res) => {
+  Blog.findById('5ea99b49b8531f40c0fde689')
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create a new Blog " });
+
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
 });
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+// blog routes
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
+
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then(result => {
+      res.render('index', { blogs: result, title: 'All blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
 // 404 page
 app.use((req, res) => {
-  res.status(404).render("404", { title: "404" });
+  res.status(404).render('404', { title: '404' });
 });
