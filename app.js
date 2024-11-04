9c79298d5acc0e47b93e90257f4a69611dc7058d
Rajaabpro <rajaab.pro@gmail.com>
2024-11-04 11:08:13 +0500

    Create blog schema with title, snippet, and body fields

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const { result } = require("lodash");
 // express app
 const app = express();
 
 // connect to mongodb
 const dbURI =
   "mongodb+srv://rajaabpro:test123@cluster0.a8mneoq.mongodb.net/node-tuts?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(dbURI);
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 // register view engine
 app.set("view engine", "ejs");
-// listen for requests
-app.listen(3000);
+
 //  middleware & static files
 app.use(express.static("public"));
 app.use(morgan("dev"));
