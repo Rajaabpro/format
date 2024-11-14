d92499192f19e8d7db7d29b414b009e0567c9626
Rajaabpro <rajaab.pro@gmail.com>
2024-11-14 12:11:25 +0500

    Refactored blog routes for modularity: extracted route handlers to blogController, enhancing readability and maintainability across blog CRUD operations

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,55 +1,15 @@
-const express = require('express');
-
+const express = require("express");
+const blogController = require("../controllers/blogcontroller");
 const router = express.Router();
+// Route to render the form for creating a new blog
+router.get("/create", blogController.blog_create_get);
+// Route to display all blogs
+router.get("/", blogController.blog_index);
+// Route to handle creating a new blog
+router.post("/", blogController.blog_create_post);
+// Route to display a specific blog's details
+router.get("/:id", blogController.blog_details);
+// Route to delete a blog by ID
+router.delete("/:id", blogController.blog_delete);
 
-router.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-  });
-  
-  router.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-      .then(result => {
-        res.render('index', { blogs: result, title: 'All blogs' });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
-  
-  router.post('/blogs', (req, res) => {
-    // console.log(req.body);
-    const blog = new Blog(req.body);
-  
-    blog.save()
-      .then(result => {
-        res.redirect('/blogs');
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
-  
-  router.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-      .then(result => {
-        res.render('details', { blog: result, title: 'Blog Details' });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
-  
-  router.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    
-    Blog.findByIdAndDelete(id)
-      .then(result => {
-        res.json({ redirect: '/blogs' });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
-  
-  module.exports = router;
+module.exports = router;
